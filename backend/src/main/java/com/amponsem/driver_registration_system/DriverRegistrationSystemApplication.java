c777dda93c36621d007ec554a32df49f50ab51a6
amponsemmichael <amponsem_michael@yahoo.com>
2025-04-17 23:04:28 +0000

    DRS: initial backend setup

---
+++ backend/src/main/java/com/amponsem/driver_registration_system/DriverRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.amponsem.driver_registration_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverRegistrationSystemApplication.class, args);
+	}
+
+}
